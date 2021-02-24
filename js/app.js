54343953a854ff52390d51c5d0429eb96324a2ab
Cullen Sharp <cullen.sharp100@gmail.com>
2021-02-24 15:27:15 -0800

    add app1.js and app.js

---
+++ js/app.js
@@ -0,0 +1,3 @@
+let username = prompt('What is your name: ');
+
+document.write('<h2>'+username+':'+'</h2>');
---
+++ js/app1.js
@@ -0,0 +1,5 @@
+if (username=='Cullen') {
+    document.write('<h2>'+'Welcome Based, '+username+'</h2>');
+} else {
+    document.write('<h2>'+username+' is not Based.'+'</h2>');
+}
