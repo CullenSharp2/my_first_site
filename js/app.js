b29ed3b0e80bcf20839413070ba570bc3b2c06ec
Cullen Sharp <cullen.sharp100@gmail.com>
2021-02-25 14:56:36 -0800

    refactor app.js, add loop

--- js/app.js
+++ js/app.js
@@ -1,29 +1,19 @@
 'use strict'
 
-function getUsername(){
-    let username = prompt('what is your name');
+function getName(){
+    let username = prompt('What is your name?');;
+    while(username === "") {
+        console.log('checking for username');
+        username = prompt('What is your name?');
+    }   
     return (username);
-}
-
-function validateName(username){
-    console.log("eval");
-    if (username !== "") {
-        return(username);
-    } else {
-        getUsername();
-        validateName();
-        console.log('username not found');
-    }
-}
-
-function writeName(username){
-    document.write(`<h3 id="username"> welcome: ${username}</h3>`);
-    console.log('success')
 }
 
 function render(username) {
     console.log(username);
-    writeName(validateName(username));
+
+    document.write(`<h3 id="username"> welcome: ${username}</h3>`);
+    console.log('success')
 }
 
 function getDate(){
